e2d20b761f394a5b0211ed3881640e7bccb10b14
Juliana Costa <jcosta@mpagg.local>
2016-09-15 15:51:50 -0300

    first commit

---
+++ RedisConnectionTest/RedisConnectorHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+
+namespace RedisConnectionTest
+{
+    public class RedisConnectorHelper 
+    {
+        static RedisConnectorHelper()
+        {
+            RedisConnectorHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect("localhost");
+            });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection 
+        {
+            get {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
